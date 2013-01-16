35a5764a239a31048bdcb7141c3e817cafccf3cb
Bogdan Artyushenko <bogdan.artyushenko@wonga.com>
2013-01-16 20:04:52 +0200

    added texter

--- src/main/java/com/ba/languagechecker/entities/WrongSentence.java
+++ src/main/java/com/ba/languagechecker/entities/WrongSentence.java
@@ -6,6 +6,48 @@ public class WrongSentence {
 
 	private UUID id;
 	private String sentence;
-	private PageCheckResult parentSentence;
-	
+	private int beginningIndex = -1;
+	private int endingIndex = -1;
+	private PageCheckResult parentPage;
+
+	public PageCheckResult getParentPage() {
+		return parentPage;
+	}
+
+	public void setParentPage(PageCheckResult parentSentence) {
+		this.parentPage = parentSentence;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public int getBeginningIndex() {
+		return beginningIndex;
+	}
+
+	public void setBeginningIndex(int beginningIndex) {
+		this.beginningIndex = beginningIndex;
+	}
+
+	public int getEndingIndex() {
+		return endingIndex;
+	}
+
+	public void setEndingIndex(int endingIndex) {
+		this.endingIndex = endingIndex;
+	}
+
 }
--- src/main/java/com/ba/languagechecker/wordchecker/TextChecker.java
+++ src/main/java/com/ba/languagechecker/wordchecker/TextChecker.java
@@ -2,16 +2,20 @@ package com.ba.languagechecker.wordchecker;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
 import com.ba.languagechecker.entities.WrongSentence;
 
 public class TextChecker {
-	private Logger logger = Logger.getLogger(TextChecker.class
-			.getCanonicalName());
+	private Logger _log = Logger
+			.getLogger(TextChecker.class.getCanonicalName());
 	private WordChecker wordChecker;
-	Pattern pattern = Pattern.compile(patternString);
+	private static final String WORD_PATTERN_EXPR = "[.,\"\'\\p{Blank}\\s][\\p{L}\\w]+[\\p{Blank}\\s.,\"\']";
+	private static final Pattern WORD_PATTERN = Pattern
+			.compile(WORD_PATTERN_EXPR);
 
 	public WordChecker getWordChecker() {
 		return wordChecker;
@@ -23,13 +27,48 @@ public class TextChecker {
 
 	public List<WrongSentence> getErrorSentences(final String text) {
 		final List<WrongSentence> wrongSentences = new LinkedList<WrongSentence>();
-		
-		Matcher matcher = pattern.matcher(text);
+		final Matcher matcher = WORD_PATTERN.matcher(text);
+
+		WrongSentence currentSentense = null;
 
 		while (matcher.find()) {
-		    System.out.println(matcher.group(1));
+			final String word = matcher.group();
+			_log.info("trying word " + matcher.group());
+			if (getWordChecker().isWordOfOriginalLanguage(word)) {
+				if (currentSentense == null) {
+					currentSentense = getCurrentSentence(matcher, word);
+				} else {
+					addNewWordToSentenc(matcher, word, currentSentense);
+				}
+			} else {
+				if (currentSentense != null) {
+					storeSentence(matcher, word, currentSentense);
+					currentSentense = null;
+				}
+			}
+
 		}
 		return wrongSentences;
 	}
 
+	private WrongSentence getCurrentSentence(final Matcher matcher,
+			final String word) {
+		final WrongSentence result = new WrongSentence();
+		result.setBeginningIndex(matcher.start());
+		result.setEndingIndex(result.getBeginningIndex() + word.length());
+		return result;
+	}
+
+	private void addNewWordToSentenc(final Matcher matcher, final String word,
+			final WrongSentence currentSentense) {
+		currentSentense.setEndingIndex(matcher.start() + word.length());
+	}
+
+	private void storeSentence(final Matcher matcher, final String word,
+			final WrongSentence currentSentense) {
+		_log.warn("new sentence  " + currentSentense.getSentence() + " at "
+				+ currentSentense.getBeginningIndex() + " ... "
+				+ currentSentense.getEndingIndex());
+
+	}
 }
