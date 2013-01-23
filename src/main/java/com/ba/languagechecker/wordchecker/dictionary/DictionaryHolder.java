d320534bf28d80876bf7396daaa10667a6ea501c
Bogdan Artyushenko <bogdan.artyushenko@wonga.com>
2013-01-23 13:04:48 +0200

    prevert dictionary from reloading the same language

--- src/main/java/com/ba/languagechecker/wordchecker/dictionary/DictionaryHolder.java
+++ src/main/java/com/ba/languagechecker/wordchecker/dictionary/DictionaryHolder.java
@@ -2,6 +2,8 @@ package com.ba.languagechecker.wordchecker.dictionary;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 
@@ -10,6 +12,8 @@ import com.ba.languagechecker.properties.TaskProperties;
 public class DictionaryHolder {
 	private Logger _log = Logger.getLogger(DictionaryHolder.class
 			.getCanonicalName());
+	private Map<String, LanguageDictionary> availableDictionaries = new HashMap<String, LanguageDictionary>(
+			2);
 	private LanguageDictionary shouldBeLanguageDictionary;
 	private LanguageDictionary originalLanguageDictionary;
 	private static final DictionaryHolder DICTIONARY_HOLDER_INSTANCE = new DictionaryHolder();
@@ -18,13 +22,30 @@ public class DictionaryHolder {
 		return DICTIONARY_HOLDER_INSTANCE;
 	}
 
+	protected DictionaryHolder() {
+		super();
+	}
+
 	public void loadDictionaries(final TaskProperties taskProperties)
 			throws FileNotFoundException, IOException {
-		originalLanguageDictionary = new LanguageDictionary(
-				taskProperties.getProperty("origin_language_code"));
-		shouldBeLanguageDictionary = new LanguageDictionary(
-				taskProperties.getProperty("shouldbe_language_code"));
+		originalLanguageDictionary = getDictionary(taskProperties
+				.getProperty("origin_language_code"));
+		shouldBeLanguageDictionary = getDictionary(taskProperties
+				.getProperty("shouldbe_language_code"));
 		_log.debug("dictionaries loaded");
+	}
+
+	private LanguageDictionary getDictionary(final String languageCode)
+			throws FileNotFoundException, IOException {
+		if (availableDictionaries.containsKey(languageCode)) {
+			_log.info(languageCode + " dictionary already loaded");
+			return availableDictionaries.get(languageCode);
+		} else {
+			_log.info("loading dictionary " + languageCode + " dictionary");
+			final LanguageDictionary dict = new LanguageDictionary(languageCode);
+			availableDictionaries.put(languageCode, dict);
+			return dict;
+		}
 	}
 
 	public LanguageDictionary getShouldBeLanguageDictionary() {
