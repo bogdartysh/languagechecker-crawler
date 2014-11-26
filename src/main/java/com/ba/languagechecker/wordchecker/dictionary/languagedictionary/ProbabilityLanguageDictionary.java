b2df3572cffb41ee6e2527f460e7183540741c52
Bogdan Artyushenko <bogdartysh@users.noreply.github.com>
2014-11-26 19:10:59 +0200

    codestyle

--- src/main/java/com/ba/languagechecker/wordchecker/dictionary/languagedictionary/ProbabilityLanguageDictionary.java
+++ src/main/java/com/ba/languagechecker/wordchecker/dictionary/languagedictionary/ProbabilityLanguageDictionary.java
@@ -3,22 +3,19 @@ package com.ba.languagechecker.wordchecker.dictionary.languagedictionary;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import com.skjegstad.utils.*;
+import com.skjegstad.utils.BloomFilter;
 
 public class ProbabilityLanguageDictionary extends AbstractLanguageDictionary implements ILanguageDictionary {
 
 	private final BloomFilter<String> bloomFilter;
-	
-	public ProbabilityLanguageDictionary(final String language,
-			final double falsePositiveProbability)
-			throws FileNotFoundException, IOException {
+
+	public ProbabilityLanguageDictionary(final String language, final double falsePositiveProbability) throws FileNotFoundException, IOException {
 		super();
 
-		bloomFilter = new BloomFilter<String>(falsePositiveProbability, MAX_AMOUNT_OF_WORDS);
-		loadDictionaryFile(PATH_TO_DICTIONARY + language
-				+ DICTIONARY_FILE_EXTENSION);
+		bloomFilter = new BloomFilter<>(falsePositiveProbability, MAX_AMOUNT_OF_WORDS);
+		loadDictionaryFile(PATH_TO_DICTIONARY + language + DICTIONARY_FILE_EXTENSION);
 	}
-	
+
 	@Override
 	public int getDictionarySize() {
 		return bloomFilter.size();
@@ -31,13 +28,12 @@ public class ProbabilityLanguageDictionary extends AbstractLanguageDictionary im
 
 	@Override
 	protected void clearDictionaries() {
-		bloomFilter.clear();		
+		bloomFilter.clear();
 	}
 
 	@Override
-	protected void addWord(final String word) {		
+	protected void addWord(final String word) {
 		bloomFilter.add(word);
 	}
-	
 
 }
